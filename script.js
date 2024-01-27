953d37f70a65237a956a00bfae474fc647170943
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-01-28 02:11:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,33 @@
 // Write your script here
 
-const progress = document.getElementById("progress");
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
 const circles = document.querySelectorAll(".circle");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+let currentIndex = 0;
 
-let currentActive = 1;
+function updateButtons() {
+  prevButton.disabled = currentIndex === 0;
+  nextButton.disabled = currentIndex === circles.length - 1;
+}
 
-next.addEventListener("click", () => {
-  currentActive++;
-
-  if (currentActive > circles.length) {
-    currentActive = circles.length;
-  }
-
-  update();
-});
-
-prev.addEventListener("click", () => {
-  currentActive--;
-
-  if (currentActive < 1) {
-    currentActive = 1;
-  }
-
-  update();
-});
-
-function update() {
-  circles.forEach((circle, idx) => {
-    if (idx < currentActive) {
-      circle.classList.add("active");
-    } else {
-      circle.classList.remove("active");
-    }
+function setActiveCircle(index) {
+  circles.forEach((circle, i) => {
+    circle.classList.toggle("active", i === index);
   });
+}
 
-  const actives = document.querySelectorAll(".active");
+function nextCircle() {
+  if (currentIndex < circles.length - 1) {
+    currentIndex++;
+    setActiveCircle(currentIndex);
+    updateButtons();
+  }
+}
 
-  progress.style.width =
-    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
-
-  if (currentActive === 1) {
-    prev.disabled = true;
-  } else if (currentActive === circles.length) {
-    next.disabled = true;
-  } else {
-    prev.disabled = false;
-    next.disabled = false;
+function prevCircle() {
+  if (currentIndex > 0) {
+    currentIndex--;
+    setActiveCircle(currentIndex);
+    updateButtons();
   }
 }
