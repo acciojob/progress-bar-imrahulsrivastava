4bd524f1b37669dd95c5ef0af3f570f0a3504aef
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-01-28 01:58:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,31 @@
 // Write your script here
 
-function next() {
-  if (targetCircle === 5) return;
-  else if (targetCircle === 4) {
-    nextButton.style.backgroundColor = "var(--non-active-color)";
-    nextButton.style.color = "rgb(178,177,175)";
+let currentNode = 1;
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+function navigate(direction) {
+  const currentElement = document.getElementById(currentNode);
+
+  if (direction === "next" && currentElement.nextElementSibling) {
+    let nextConnection = currentElement.nextElementSibling;
+    nextConnection.classList.add("active");
+    let nextNode = currentElement.nextElementSibling.nextElementSibling;
+    nextNode.classList.add("active");
+    currentNode++;
+  } else if (direction === "prev" && currentElement.previousElementSibling) {
+    currentElement.classList.remove("active");
+    currentElement.previousElementSibling.classList.remove("active");
+    currentNode--;
   }
-
-  if (targetCircle === 1) {
-    prevButton.style.backgroundColor = "var(--active-color)";
-    prevButton.style.color = "whitesmoke";
-  }
-
-  circles[targetCircle].classList.toggle("active");
-  connections[targetCircle - 1].style.backgroundColor = "var(--active-color)";
-
-  prevButton.disabled = targetCircle <= 1 ? true : false;
-
-  if (targetCircle <= 4) targetCircle++;
+  currentNode === 5 ? (next.disabled = true) : (next.disabled = false);
+  currentNode === 1 ? (prev.disabled = true) : (prev.disabled = false);
 }
 
-function prev() {
-  if (targetCircle === 1) return;
-  else if (targetCircle === 5) {
-    nextButton.style.background = "var(--active-color)";
-    nextButton.style.color = "whitesmoke";
-  }
+next.addEventListener("click", () => {
+  navigate("next");
+});
 
-  if (targetCircle === 2) {
-    prevButton.style.background = "var(--non-active-color)";
-    prevButton.style.color = "rgb(178,177,175)";
-  }
-
-  circles[targetCircle - 1].classList.toggle("active");
-  connections[targetCircle - 2].style.background = "var(--non-active-color)";
-
-  nextButton.disabled = targetCircle >= 5 ? true : false;
-
-  if (targetCircle >= 2) targetCircle--;
-}
-
-const nextButton = document.getElementById("next");
-const prevButton = document.getElementById("prev");
-
-let targetCircle = 1;
-
-const circles = document.querySelectorAll(".circle");
-const connections = document.querySelectorAll(".connection");
-
-nextButton.addEventListener("click", next);
-prevButton.addEventListener("click", prev);
+prev.addEventListener("click", () => {
+  navigate("prev");
+});
